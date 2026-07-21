259935433a79137bff6fa3a7466349e850abca11
NotMarco97 <ma.marcoadame@gmail.com>
2026-07-21 09:38:14 -0500

    Implement configuration loading and validation. Added settings credentials

--- src/config.ps1
+++ src/config.ps1
@@ -1,6 +1,6 @@
 function GetConfiguration{
-    $check = Test-Path 
-    $content = Get-content 
+    $check = Test-Path /home/marco/Desktop/powershell/kiosk-watchdog/config/settings.json
+    $content = Get-content /home/marco/Desktop/powershell/kiosk-watchdog/config/settings.json
 
     $configuration = $content | ConvertFrom-json
 
