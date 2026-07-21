87f91a0e50910f45dadfab1069a5162f480fd0ad
NotMarco97 <ma.marcoadame@gmail.com>
2026-07-21 10:24:06 -0500

    Updated config path to settings.json

--- src/config.ps1
+++ src/config.ps1
@@ -1,8 +1,20 @@
 function GetConfiguration{
     $check = Test-Path /home/marco/Desktop/powershell/kiosk-watchdog/config/settings.json
-    $content = Get-content /home/marco/Desktop/powershell/kiosk-watchdog/config/settings.json
+    
+    if($check){
+        $content = Get-content /home/marco/Desktop/powershell/kiosk-watchdog/config/settings.json
+        $configuration = $content | ConvertFrom-json
+    }else {
+        throw "Configuration file not found."
+    }
 
-    $configuration = $content | ConvertFrom-json
+    $isUsernameInvalid = [string]::IsNullOrEmpty($configuration.username)
+    $isPasswordInvalid = [string]::IsNullOrEmpty($configuration.password)
+    
+    if($isUsernameInvalid -or $isPasswordInvalid){
+
+        throw "Username or Password is not valid."
+    }
 
     return $configuration
 }
